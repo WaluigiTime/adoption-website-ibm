d04e0fd070214cb8fdae006d35af16763d3516fa
WaluigiTime <43816974+WaluigiTime@users.noreply.github.com>
2022-07-27 15:33:45 -0400

    Initial commit

---
+++ src/MainPage.jsx
@@ -0,0 +1,24 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="MainPage">
+      <div className="Title">
+        <h1>
+          <u>
+            <i> AdoptDo </i>
+          </u>
+        </h1>
+
+        <hr />
+        <p>
+          <a href="https://google.com"> Match Info </a> |
+          <a href="https://google.com"> Personality Test </a> |
+          <a href="https://google.com"> Faculty and Questions </a>
+        </p>
+      </div>
+
+      <div className="AboutUs">Adopt children please.</div>
+    </div>
+  );
+}
